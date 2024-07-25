029353b2e18c8d33bfc0f1a85b22b0fc0ae5ca67
Caique <asapcaique7@gmail.com>
2024-07-25 19:11:48 -0300

    configurando rest api

--- projects/go-movies-crud/main.go
+++ projects/go-movies-crud/main.go
@@ -24,6 +24,27 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().set("content-type", "application/json")
+	json.encoder(w).Encode(movies)
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "438227", Title: "LyfeStyle", Director : &Director(
+		FirstName: "Caique", 
+	LastName: "Gonçalves")})
+
+	movies = append(movies, Movie{ID: "2", Isbn: "468227", Title: "Pissed", Director : &Director(
+		FirstName: "Caique", 
+	LastName: "Gonçalves")})
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.fatal(http.ListenAndServe(":8080", r))
 }
